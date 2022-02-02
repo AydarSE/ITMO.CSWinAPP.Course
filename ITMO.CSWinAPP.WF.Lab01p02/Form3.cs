24b65442e09ad17368a39d560c8652639af39868
AydarSE <Galiev_aidar@mail.ru>
2022-02-02 23:07:56 +0300

    Add WF Lab01p02

---
+++ ITMO.CSWinAPP.WF.Lab01p02/Form3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WFLab1._2
+{
+    public partial class Form3 : WFLab1._2.Form1
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+    }
+}
